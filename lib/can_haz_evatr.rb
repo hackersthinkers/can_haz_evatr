ed35b067ae3dc8361f60cda252edad92d1d4a0ff
Lars Brillert <lars@hackersthinkers.com>
2021-03-12 06:07:53 +0100

    initial

---
+++ lib/can_haz_evatr.rb
@@ -0,0 +1,6 @@
+require "can_haz_evatr/version"
+
+module CanHazEvatr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/can_haz_evatr/version.rb
@@ -0,0 +1,3 @@
+module CanHazEvatr
+  VERSION = "0.1.0"
+end
